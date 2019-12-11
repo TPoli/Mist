b6fc5791b3c4d834f50327c361a544b9a9f900e9
TPoli <thomaspollinger72@gmail.com>
2019-12-12 01:41:35 +1100

    First commit

---
+++ renderable.js
@@ -0,0 +1,10 @@
+
+const Renderable = (a_iX, a_iY) => {
+
+    const renderable = {
+        X: a_iX,
+        Y: a_iY
+    }
+
+    return renderable;
+}
---
+++ shader.js
@@ -0,0 +1,48 @@
+
+var m_oDefaultShader = null;
+
+const CreateShader = (a_sVert, a_sFrag) => {
+    var gl = canvas.getContext('experimental-webgl');
+
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, a_sVert);
+    gl.compileShader(vertShader);
+
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, a_sFrag);
+    gl.compileShader(fragShader);
+
+    var shader = gl.createProgram();
+
+    gl.attachShader(shader, vertShader); 
+    gl.attachShader(shader, fragShader);
+
+    gl.linkProgram(shader);
+
+    return shader;
+}
+
+const DefaultShader = () => {
+    if(m_oDefaultShader !== null) {
+        return m_oDefaultShader;
+    }
+
+    var vertCode = 'attribute vec3 coordinates;'+
+            'attribute vec3 color;' +
+            'varying vec3 vColor;' +
+            'uniform vec2 position;' +
+            'void main(void) {' +
+                ' gl_Position = vec4(coordinates + vec3(position,0), 1.0);' +
+               'vColor = color;'+
+            '}';
+
+            var fragCode = 'precision mediump float;'+
+            'varying vec3 vColor;'+
+            'void main(void) {'+
+               'gl_FragColor = vec4(vColor, 1.);'+
+            '}';
+
+    m_oDefaultShader = CreateShader(vertCode, fragCode);
+    
+    return m_oDefaultShader;
+}
