80669b9bb4b9022e22ae580d4127cb6c65d7fa40
Thomas Pollinger <Grimm_wolf@live.com.au>
2019-12-12 03:26:10 +1100

    changed main.js to index.js. added eslint. linted code. added http-server script. Moved placeholder code to main.js.

--- renderable.js
+++ renderable.js
@@ -1,10 +1,10 @@
 
-const Renderable = (a_iX, a_iY) => {
+export const Renderable = (a_iX, a_iY) => {
 
-    const renderable = {
-        X: a_iX,
-        Y: a_iY
-    }
+	const renderable = {
+		X: a_iX,
+		Y: a_iY
+	};
 
-    return renderable;
-}
+	return renderable;
+};
--- shader.js
+++ shader.js
@@ -1,33 +1,33 @@
-
 var m_oDefaultShader = null;
 
 const CreateShader = (a_sVert, a_sFrag) => {
-    var gl = canvas.getContext('experimental-webgl');
+	var canvas = document.getElementById('my_Canvas');
+	var gl = canvas.getContext('experimental-webgl');
 
-    var vertShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertShader, a_sVert);
-    gl.compileShader(vertShader);
+	var vertShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertShader, a_sVert);
+	gl.compileShader(vertShader);
 
-    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragShader, a_sFrag);
-    gl.compileShader(fragShader);
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragShader, a_sFrag);
+	gl.compileShader(fragShader);
 
-    var shader = gl.createProgram();
+	var shader = gl.createProgram();
 
-    gl.attachShader(shader, vertShader); 
-    gl.attachShader(shader, fragShader);
+	gl.attachShader(shader, vertShader); 
+	gl.attachShader(shader, fragShader);
 
-    gl.linkProgram(shader);
+	gl.linkProgram(shader);
 
-    return shader;
-}
+	return shader;
+};
 
-const DefaultShader = () => {
-    if(m_oDefaultShader !== null) {
-        return m_oDefaultShader;
-    }
+export const DefaultShader = () => {
+	if(m_oDefaultShader !== null) {
+		return m_oDefaultShader;
+	}
 
-    var vertCode = 'attribute vec3 coordinates;'+
+	var vertCode = 'attribute vec3 coordinates;'+
             'attribute vec3 color;' +
             'varying vec3 vColor;' +
             'uniform vec2 position;' +
@@ -36,13 +36,13 @@ const DefaultShader = () => {
                'vColor = color;'+
             '}';
 
-            var fragCode = 'precision mediump float;'+
+	var fragCode = 'precision mediump float;'+
             'varying vec3 vColor;'+
             'void main(void) {'+
                'gl_FragColor = vec4(vColor, 1.);'+
             '}';
 
-    m_oDefaultShader = CreateShader(vertCode, fragCode);
+	m_oDefaultShader = CreateShader(vertCode, fragCode);
     
-    return m_oDefaultShader;
-}
+	return m_oDefaultShader;
+};
